ddcc9832fdc05681d1e80959f136a29582560f4c
HelloWorld <nurhidayah282000@gmail.com>
2021-10-12 20:50:26 +0700

    Layout Register

---
+++ app/src/main/java/com/example/mytodolist/RegisterActivity.java
@@ -0,0 +1,85 @@
+package com.example.mytodolist.Register;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mytodolist.Login.LoginActivity;
+import com.example.mytodolist.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText etUsername, etPassword, etConfPassword;
+    Button btnRegister;
+    DBHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        dbHelper = new DBHelper(this);
+
+        etUsername = (EditText)findViewById(R.id.et_username_reg);
+        etPassword = (EditText)findViewById(R.id.et_password_reg);
+        etConfPassword = (EditText)findViewById(R.id.et_password_confirmation);
+        btnRegister = (Button)findViewById(R.id.btn_register);
+
+        TextView tvRegister = (TextView)findViewById(R.id.tvRegister);
+
+        tvRegister.setText(fromHtml("Sudah punya akun? " +
+                "</font><font color='#3b5998'>Login</font>"));
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+                String conPassword = etConfPassword.getText().toString().trim();
+
+                ContentValues values = new ContentValues();
+
+
+                if (!password.equals(conPassword)){
+                    Toast.makeText(RegisterActivity.this, "Password tidak valid", Toast.LENGTH_SHORT).show();
+                }else if (password.equals("") || username.equals("")){
+                    Toast.makeText(RegisterActivity.this, "Nama dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                }else {
+                    values.put(DBHelper.ROW_USERNAME, username);
+                    values.put(DBHelper.ROW_PASSWORD, password);
+                    dbHelper.insertData(values);
+
+                    Toast.makeText(RegisterActivity.this, "Register berhasil", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+    }
+
+    public static Spanned fromHtml(String html){
+        Spanned result;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        }else {
+            result = Html.fromHtml(html);
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/mytodolist/Utils/DBHelper.java
@@ -0,0 +1,58 @@
+package com.example.mytodolist.Utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "db_login";
+    public static final String TABLE_NAME = "table_login";
+
+    public static final String ROW_ID = "_id";
+    public static final String ROW_USERNAME = "Username";
+    public static final String ROW_PASSWORD = "Password";
+
+    private SQLiteDatabase db;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 2);
+        db = getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME + "(" + ROW_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + ROW_USERNAME + " TEXT," + ROW_PASSWORD + " TEXT)";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+    }
+
+    //Insert Data
+    public void insertData(ContentValues values){
+        db.insert(TABLE_NAME, null, values);
+    }
+
+
+    public boolean checkUser(String username, String password){
+        String[] columns = {ROW_ID};
+        SQLiteDatabase db = getReadableDatabase();
+        String selection = ROW_USERNAME + "=?" + " and " + ROW_PASSWORD + "=?";
+        String[] selectionArgs = {username,password};
+        Cursor cursor = db.query(TABLE_NAME, columns, selection, selectionArgs, null, null, null);
+        int count = cursor.getCount();
+        cursor.close();
+        db.close();
+
+        if (count>0)
+            return true;
+        else
+            return false;
+    }
+
+}
